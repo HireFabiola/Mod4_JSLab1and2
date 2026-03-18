d2e7f1b952bcddfa23cfb85b9c6cd10598850ddd
Fabiola <fabiola.aurelien@gmail.com>
2026-03-18 16:26:43 -0400

    Added code to change first letters of first and last names to capital letters

--- Lab1/index.js
+++ Lab1/index.js
@@ -1,8 +1,13 @@
-let fullName = "Fabiola Aurelien";
+let fullName = "fabiola aurelien";
 
-function formatFullName(nameString){
+function formatFullName(nameString) {
     const stringArray = nameString.split(" ")
-    console.log(stringArray[1] + ", " + stringArray[0])
-    }
+    let capFirstLetterofFirst = stringArray[0].charAt(0).toUpperCase() + stringArray[0].slice(1);
+    let capFirstLetterofLast = stringArray[1].charAt(0).toUpperCase() + stringArray[1].slice(1);
+
+    console.log(capFirstLetterofFirst);
+    console.log(capFirstLetterofLast);
+    console.log(capFirstLetterofLast + ", " + stringArray[0])
+}
 
 formatFullName(fullName);
