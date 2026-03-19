7f983593868bc22f95b693dbde587b9caa2c6aa3
Fabiola <fabiola.aurelien@gmail.com>
2026-03-18 22:53:39 -0400

    Added a case to cj=heck for more than 2 names.  Now Im done with task1

--- Lab1/Task1/index.js
+++ Lab1/Task1/index.js
@@ -1,33 +1,35 @@
-let fullName = "fabiola aurelien";
+let fullName = "Fabiola aurelien";
 
 function formatFullName(nameString) {
 
     if (hasMoreThanOneWord(nameString)) {
 
-        const stringArray = nameString.split(" ")
-        let capFirstLetterofFirst = stringArray[0].charAt(0).toUpperCase() + stringArray[0].slice(1);
-        let capFirstLetterofLast = stringArray[1].charAt(0).toUpperCase() + stringArray[1].slice(1);
-        // console.log(capFirstLetterofFirst);
-        // console.log(capFirstLetterofLast);
-  
-    console.log(capFirstLetterofLast + ", " + capFirstLetterofFirst);
+        const stringArray = nameString.trim().split(/\s+/);
+
+        if (stringArray.length > 2) {
+            console.log("Invalid name input");
+        } else {
+            let capFirstLetterofFirst = stringArray[0].charAt(0).toUpperCase() + stringArray[0].slice(1);
+            let capFirstLetterofLast = stringArray[1].charAt(0).toUpperCase() + stringArray[1].slice(1);
+
+            console.log(capFirstLetterofLast + ", " + capFirstLetterofFirst);
+        }
+
+    } else {
+        console.log('Invalid name input');
     }
-    else{
-    // if ((stringArray[0] == "") || (stringArray[1] == ""))
-    console.log('Invalid name input');
-}
 }
 
-//Function taken verbatim from internet search
-function hasMoreThanOneWord(inputString) {
-    // Use trim() to remove leading/trailing spaces
-    const trimmedString = inputString.trim();
+    //Function taken verbatim from internet search
+    function hasMoreThanOneWord(inputString) {
+        // Use trim() to remove leading/trailing spaces
+        const trimmedString = inputString.trim();
 
-    // Split the string by one or more whitespace characters (/\s+/)
-    const words = trimmedString.split(/\s+/);
+        // Split the string by one or more whitespace characters (/\s+/)
+        const words = trimmedString.split(/\s+/);
 
-    // Return true if the array length is greater than 1
-    return words.length > 1;
-}
+        // Return true if the array length is greater than 1
+        return words.length > 1;
+    }
 
-formatFullName(fullName);
+    formatFullName(fullName);
