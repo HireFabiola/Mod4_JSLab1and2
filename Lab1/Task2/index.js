8a7e647ec3a1e71508fdd9c4274d225ef1a78b48
Fabiola <fabiola.aurelien@gmail.com>
2026-03-18 23:24:37 -0400

    completed task 2 with checks for validity of inputs and added rounding to hundredths place

--- Lab1/Task2/index.js
+++ Lab1/Task2/index.js
@@ -1,6 +1,14 @@
-function calculateTotalCost(price, quantity, taxRate){
-    let totalCost = (price * quantity) * (1+taxRate)
-    console.log(totalCost);
+function calculateTotalCost(price, quantity, taxRate) {
+    if ((typeof price === "number" && !Number.isNaN(price)) &&
+        (typeof quantity === "number" && !Number.isNaN(quantity)) &&
+        (typeof taxRate === "number" && !Number.isNaN(taxRate))) {
+        let totalCost = (price * quantity) * (1 + taxRate)
+        console.log(totalCost.toFixed(2));
+    }
+    else {
+        console.log('Invalid input');
+    }
 }
 
-calculateTotalCost(2.50,1,.10)
+calculateTotalCost(3, 1, .10)
+
