c32371edfdf74765efbee6ce26f6414e041b8975
Fabiola <fabiola.aurelien@gmail.com>
2026-03-18 21:02:43 -0400

    Reorged my files/  Deleted extra index.js file from outer folder

---
+++ Lab1/Task1/index.js
@@ -0,0 +1,33 @@
+let fullName = "fabiola aurelien";
+
+function formatFullName(nameString) {
+
+    if (hasMoreThanOneWord(nameString)) {
+
+        const stringArray = nameString.split(" ")
+        let capFirstLetterofFirst = stringArray[0].charAt(0).toUpperCase() + stringArray[0].slice(1);
+        let capFirstLetterofLast = stringArray[1].charAt(0).toUpperCase() + stringArray[1].slice(1);
+        // console.log(capFirstLetterofFirst);
+        // console.log(capFirstLetterofLast);
+  
+    console.log(capFirstLetterofLast + ", " + capFirstLetterofFirst);
+    }
+    else{
+    // if ((stringArray[0] == "") || (stringArray[1] == ""))
+    console.log('Invalid name input');
+}
+}
+
+//Function taken verbatim from internet search
+function hasMoreThanOneWord(inputString) {
+    // Use trim() to remove leading/trailing spaces
+    const trimmedString = inputString.trim();
+
+    // Split the string by one or more whitespace characters (/\s+/)
+    const words = trimmedString.split(/\s+/);
+
+    // Return true if the array length is greater than 1
+    return words.length > 1;
+}
+
+formatFullName(fullName);
